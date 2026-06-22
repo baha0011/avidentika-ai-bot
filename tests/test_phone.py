a45c66a1bddae776fe03b695b028870030d1e6cb
baha0011 <102609988+baha0011@users.noreply.github.com>
2026-06-22 19:18:23 +0300

    Cover flexible website phone inputs

--- tests/test_phone.py
+++ tests/test_phone.py
@@ -9,6 +9,14 @@ def test_accepts_international_ua_phone() -> None:
     assert normalize_ua_phone("+380 (44) 123-45-67") == "+380441234567"
 
 
+def test_accepts_flexible_website_phone_formats() -> None:
+    assert normalize_ua_phone("+380 99 561 48 49") == "+380995614849"
+    assert normalize_ua_phone("+38 (099)561-67-34") == "+380995616734"
+    assert normalize_ua_phone("099.561.48.49") == "+380995614849"
+    assert normalize_ua_phone("00380 99 561 48 49") == "+380995614849"
+    assert normalize_ua_phone("80 99 561 48 49") == "+380995614849"
+
+
 def test_rejects_invalid_phone() -> None:
     assert normalize_ua_phone("+48123123123") is None
     assert normalize_ua_phone("067123") is None
